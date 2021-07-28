2b624dce686854776dbc071a8518af7794f4df06
Paulo Correa (DELL) <henriqueph@hotmail.com>
2021-07-28 18:45:18 -0300

    Criação de teste para UsuarioRepository

---
+++ src/test/java/br/com/phmr/mf/model/repository/UsuarioRepositoryTest.java
@@ -0,0 +1,30 @@
+package br.com.phmr.mf.model.repository;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import br.com.phmr.mf.model.entity.Usuario;
+
+@SpringBootTest
+@ExtendWith(SpringExtension.class)
+public class UsuarioRepositoryTest {
+
+	@Autowired
+	UsuarioRepository repository;
+
+	@Test
+	public void verificarExistenciaEmail() {
+		// Cenário
+		Usuario usuario = Usuario.builder().nome("Teste").senha("Teste").email("teste@gmail.com").build();
+		repository.save(usuario);
+		// Execução
+		boolean result = repository.existsByEmail("teste@gmail.com");
+		// Verificação
+		Assertions.assertThat(result).isTrue();
+	}
+
+}
