ff1eb48b9483c22d33105b3cd4baed81681f1325
Paulo Correa (DELL) <henriqueph@hotmail.com>
2021-09-01 21:33:27 -0300

    Implementação de POST de Usuario

---
+++ src/main/java/br/com/phmr/mf/api/resource/UsuarioResource.java
@@ -0,0 +1,34 @@
+package br.com.phmr.mf.api.resource;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.phmr.mf.api.dto.UsuarioDTO;
+import br.com.phmr.mf.model.entity.Usuario;
+import br.com.phmr.mf.service.UsuarioService;
+
+@RestController
+@RequestMapping("/api/usuarios")
+public class UsuarioResource {
+
+	private UsuarioService service;
+
+	public UsuarioResource(UsuarioService service) {
+		this.service = service;
+	}
+
+	@PostMapping
+	public ResponseEntity salvar(@RequestBody UsuarioDTO dto) {
+		Usuario usuario = Usuario.builder().nome(dto.getNome()).email(dto.getEmail()).senha(dto.getSenha()).build();
+		try {
+			Usuario usuarioSalvo = service.salvarUsuario(usuario);
+			return new ResponseEntity(usuarioSalvo, HttpStatus.CREATED);
+		} catch (Exception ex) {
+			return ResponseEntity.badRequest().body(ex.getMessage());
+		}
+	}
+}
